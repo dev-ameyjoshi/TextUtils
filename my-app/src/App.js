2751c9bd1cd9d3a1135aa2c0b5ad08e525a1a7f3
Amey Joshi <ameyjoshi2001rj@gmail.com>
2021-09-14 23:18:13 +0530

    initial practice

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+let name ="Amey";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Amey
-        </a>
-      </header>
+    <>
+    <nav className = "navbar">
+      <li>
+       Home
+      </li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <img src = " " alt = "" />
+    <div className = "container">
+    <h4>My name is {name}</h4>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et perspiciatis, dolorum enim aut nam fugit doloremque vero sint soluta magni nesciunt, iusto sapiente laborum? Molestias, nisi quibusdam. Esse, ipsa sunt!</p>
     </div>
+    </>
   );
 }
 
